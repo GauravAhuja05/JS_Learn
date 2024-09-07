e3683b1da802f27957b4f888f75e135f123e366f
GauravAhuja05 <gaurava945@gmail.com>
2024-09-07 12:33:11 +0000

    update Note in Comment section

--- LearnJS/01_Basic_JS/01_test.js
+++ LearnJS/01_Basic_JS/01_test.js
@@ -4,4 +4,20 @@ let email = "GauravA945@gmail.com";
 var name = "Gaurav"
 let NewFilename
 
+/*
+preffer not to use var
+because of issue in block scope  and fuctional scope
+
+Output will be 
+
+Hey Gaurav
+┌─────────┬────────────────────────┐
+│ (index) │ Values                 │
+├─────────┼────────────────────────┤
+│ 0       │ 'GauravA945@gmail.com' │
+│ 1       │ 'Gaurav'               │
+│ 2       │ undefined              │
+└─────────┴────────────────────────┘
+*/
+
 console.table([email, name, NewFilename])
