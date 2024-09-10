1b3bb14d8a5c4e344e43370eebac4e0f4556016c
GauravAhuja05 <gaurava945@gmail.com>
2024-09-10 07:58:30 +0000

    update datatype and creat file converstionOperation

--- Learn_JS/02_dataTypes.js
+++ Learn_JS/02_dataTypes.js
@@ -1,6 +1,10 @@
 "use strict"; // treat all JS code as newer version
 
 // alert (3+3) // we are using node.js, Not Browser
-
+// Javascript documentation at https://tc39.es/ecma262/#sec-into
+// mdn document
 console.log(3+3);
 console.log("Gaurav")
+
+console.log(typeof undefined);
+console.log(typeof null);
