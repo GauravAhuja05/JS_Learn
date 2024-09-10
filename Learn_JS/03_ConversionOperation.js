753f8a44efd69f12b4ec0fcdd0f328db7f5988ed
GauravAhuja05 <gaurava945@gmail.com>
2024-09-10 08:52:16 +0000

    completed Conversion and Operations

---
+++ Learn_JS/03_ConversionOperation.js
@@ -0,0 +1,39 @@
+//let score = 33; // will get a number
+// let score = "33abc"; //will get NaN
+// let score ="Gaurav Ahuja";//will get NaN
+let score =undefined;//will get NaN
+
+/*
+Notes : 
+33 =>  33
+33abc => NaN
+true => 1 , false => 0
+undefined => NaN
+*/ 
+
+let valueNumber = Number(score);
+
+console.log(typeof score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+
+console.log("--\n------------");
+
+let isBolean ="Gaurav";
+let BoleanIsLogedin = Boolean(isBolean);
+
+console.log(BoleanIsLogedin);
+console.log(typeof BoleanIsLogedin);
+
+
+
+
+/**
+ * Notes:  
+ * -----------------
+ *  1 => true, 0 => false
+ * "" =>  false
+ * "Gaurav" => true
+ * 
+ */
