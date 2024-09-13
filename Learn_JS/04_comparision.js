ea09d8b43415658ae468f206446f1f9beb7ca230
GauravAhuja05 <gaurava945@gmail.com>
2024-09-13 07:58:54 +0000

    datatype Summery stack and heap

---
+++ Learn_JS/04_comparision.js
@@ -0,0 +1,19 @@
+console.log("----Different datatype ---");
+console.log("2">1);
+console.log("02">1);
+
+
+console.log("----Comparision with null---");
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(null==0);
+console.log(null>0);
+console.log(null<0);
+
+
+console.log("----Strict comparisiojn---");
+// === strict check  it check value and datatype as wll
+console.log("2"===2);
+console.log("2"==="2");
---
+++ Learn_JS/datatype-summary.js
@@ -0,0 +1,23 @@
+// #1 Primtive datatype
+
+// 7 types : String, Number, Boolean, null, 
+//            undefined , Symbol, BigInt
+
+// Get Copy of : (Primtive datatype) ,  (Storage: Stack)
+// Get Reference of : ( Non primitive datatype ) , ( Storage : Heap ): Array, Objects , function
+
+// Mater in javascript -> Objects and Borwer web events.
+//JavaScript is dynamically typed, meaning that the type of a variable is determined at runtime, not at compile-time. You don't need to specify the data type of a variable when you declare it, and a variable's type can change as the program runs. For example:
+let x = 5;       // x is a number
+x = "hello";     // now x is a string
+console.log(x);
+//In JavaScript, you can assign different types to the same variable without needing to declare a new one, which is characteristic of dynamically typed languages.
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn =  false;
+const outsideTemp =null;
+let userEmail; // let userEmail = undefined  both are same 
