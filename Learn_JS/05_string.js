ffc0ecc1f886c5b3467e3c95e47cff8ac28d5e46
GauravAhuja05 <gaurava945@gmail.com>
2024-09-13 13:04:49 +0000

    maths fun

---
+++ Learn_JS/05_string.js
@@ -0,0 +1,13 @@
+const GameName = new String("Gaurav Ahuja");
+
+
+// console.log(GameName.charAt(5));
+
+
+const newVar1 = GameName.substring(0,2)
+console.log(newVar1);
+
+
+const newVar = GameName.slice(-5,0)
+console.log(newVar);
+
---
+++ Learn_JS/06_num_and_math.js
@@ -0,0 +1,35 @@
+const score = 400;
+// console.log(score);
+
+
+const Balance = new Number(100);
+// console.log(Balance);
+
+// console.log(Balance.toString())
+// console.log(Balance.toString().length);
+
+// console.log(Balance.toFixed(2)); // to show in decimal
+
+
+//toPrecision   
+//toPrecision(precision?: number): string Number of significant digits. Must be in the range 1 - 21, inclusive.
+//Returns a string containing a number represented either in exponential or fixed-point notation with a specified number of digits.
+const OtherNum = 455.65;
+// console.log(OtherNum.toPrecision())
+
+const hundredNum = 1000000;
+// console.log(hundredNum.toLocaleString());
+// console.log(hundredNum.toLocaleString('en-IN'));
+
+const num = Number(100);
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+
+//++++++++++++++++++++++++ MATCHS  ++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math);  
+console.log(Math.abs(-4));  
+console.log(Math.round(5.3));
+console.log(Math.ceil(5.3));
+console.log(Math.floor(5.8));
